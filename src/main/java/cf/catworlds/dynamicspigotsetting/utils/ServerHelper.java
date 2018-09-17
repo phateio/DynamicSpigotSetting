489693fd1cd5c284b331c933ec84b29db2a6a5b4
WhiteCat <whitecatx6@gmail.com>
2018-09-17 13:42:44 +0800

    Update Utils to 1.13.1

--- src/main/java/cf/catworlds/dynamicspigotsetting/utils/ServerHelper.java
+++ src/main/java/cf/catworlds/dynamicspigotsetting/utils/ServerHelper.java
@@ -4,28 +4,28 @@ public class ServerHelper {
 
 	@SuppressWarnings("deprecation")
 	static public double getTPS() {
-		return net.minecraft.server.v1_12_R1.MinecraftServer.getServer().recentTps[0];
+		return net.minecraft.server.v1_13_R2.MinecraftServer.getServer().recentTps[0];
 	}
 
 	@SuppressWarnings("deprecation")
 	static public double getItemMerge() {
-		return net.minecraft.server.v1_12_R1.MinecraftServer.getServer().worlds.get(0).spigotConfig.itemMerge;
+		return net.minecraft.server.v1_13_R2.MinecraftServer.getServer().getWorlds().iterator().next().spigotConfig.itemMerge;
 	}
 
 	@SuppressWarnings("deprecation")
 	static public void setItemMerge(double radius) {
-		net.minecraft.server.v1_12_R1.MinecraftServer.getServer().worlds
+		net.minecraft.server.v1_13_R2.MinecraftServer.getServer().getWorlds()
 				.forEach((ws) -> ws.spigotConfig.itemMerge = radius);
 	}
 
 	@SuppressWarnings("deprecation")
 	static public double getExpMerge() {
-		return net.minecraft.server.v1_12_R1.MinecraftServer.getServer().worlds.get(0).spigotConfig.expMerge;
+		return net.minecraft.server.v1_13_R2.MinecraftServer.getServer().getWorlds().iterator().next().spigotConfig.expMerge;
 	}
 
 	@SuppressWarnings("deprecation")
 	static public void setExpMerge(double radius) {
-		net.minecraft.server.v1_12_R1.MinecraftServer.getServer().worlds
+		net.minecraft.server.v1_13_R2.MinecraftServer.getServer().getWorlds()
 				.forEach((ws) -> ws.spigotConfig.expMerge = radius);
 	}
 
