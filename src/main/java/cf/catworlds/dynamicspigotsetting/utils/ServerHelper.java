d77674eaa01bc254c8ab7477021e4a0b194d6a20
WhiteCat <whitecatx6@gmail.com>
2020-11-08 16:48:54 +0800

    version 1.3 for mc 1.16.3+

--- src/main/java/cf/catworlds/dynamicspigotsetting/utils/ServerHelper.java
+++ src/main/java/cf/catworlds/dynamicspigotsetting/utils/ServerHelper.java
@@ -1,6 +1,6 @@
 package cf.catworlds.dynamicspigotsetting.utils;
 
-import net.minecraft.server.v1_16_R1.MinecraftServer;
+import net.minecraft.server.v1_16_R3.MinecraftServer;
 
 public class ServerHelper {
 
