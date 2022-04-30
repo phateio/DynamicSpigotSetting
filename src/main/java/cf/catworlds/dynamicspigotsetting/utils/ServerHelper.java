9bcd1e0fd9b49605323a41646af93867a5f39885
WhiteCat <whitecatx6@gmail.com>
2022-04-30 20:18:30 +0800

    Upgrade code to 1.18.2

--- src/main/java/cf/catworlds/dynamicspigotsetting/utils/ServerHelper.java
+++ src/main/java/cf/catworlds/dynamicspigotsetting/utils/ServerHelper.java
@@ -1,6 +1,6 @@
 package cf.catworlds.dynamicspigotsetting.utils;
 
-import net.minecraft.server.v1_16_R3.MinecraftServer;
+import net.minecraft.server.MinecraftServer;
 
 public class ServerHelper {
 
@@ -11,23 +11,23 @@ public class ServerHelper {
 
     @SuppressWarnings("deprecation")
     static public double getItemMerge() {
-        return MinecraftServer.getServer().getWorlds().iterator().next().spigotConfig.itemMerge;
+        return MinecraftServer.getServer().levels.values().iterator().next().spigotConfig.itemMerge;
     }
 
     @SuppressWarnings("deprecation")
     static public void setItemMerge(double radius) {
-        MinecraftServer.getServer().getWorlds()
+        MinecraftServer.getServer().levels.values()
                 .forEach((ws) -> ws.spigotConfig.itemMerge = radius);
     }
 
     @SuppressWarnings("deprecation")
     static public double getExpMerge() {
-        return MinecraftServer.getServer().getWorlds().iterator().next().spigotConfig.expMerge;
+        return MinecraftServer.getServer().levels.values().iterator().next().spigotConfig.expMerge;
     }
 
     @SuppressWarnings("deprecation")
     static public void setExpMerge(double radius) {
-        MinecraftServer.getServer().getWorlds()
+        MinecraftServer.getServer().levels.values()
                 .forEach((ws) -> ws.spigotConfig.expMerge = radius);
     }
 
